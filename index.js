a6a15339301a40987395728e8e35a47848c6a6f4
Mark Ghaida <markghaida@gmail.com>
2021-01-11 21:53:12 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,39 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet (string) {
+
+  let stringToInt = string.split(":");
+  let time= parseInt(stringToInt, 10); 
+  
+
+  if (time < 12) {
+
+    return "Good Morning";
+  }
+  
+  else if (time === 12) {
+    return "Good Afternoon";
+  }
+ 
+  else if (time > 12 && time < 17){
+
+    return "Good Afternoon";
+  }
+  
+  else if (time === 17) {
+    return "Good Evening";
+  }
+
+  else if (time > 17) {
+    return "Good Evening";
+  }
+
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage (input){
+  document.getElementById("greeting").innerText = input;
+}
